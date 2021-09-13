1dd1da4ba649b1042539cb70b8f9b02519893b79
mrralphpierre <mrralphpierre@gmail.com>
2021-09-13 11:35:54 -0400

    added db folder, containing connection.js page, as well as the required sql files, db, schema, and seeds files.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # employee-tracker
+
+## Description
+This is a command-line application built to help manage a company's employee database, using Node.js, Inquirer, and My SQL.
+
+
+## Table of Contents
+
+## Usage
+
+## Contributors
+
+
+## Tests
+![GitHub license](https://img.shields.io/badge/test-100%25-success)
+## Technology Used
---
+++ db/schema.sql
@@ -0,0 +1,106 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE database employee_tracker_db;
+
+USE employee_tracker_db;
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT(10)
+);
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30),
+    department_id INT(10)
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT(10),
+    manager_id INT(10) NULL
+);
+
+
+
+
+
+
+
+
+-- DROP TABLE IF EXISTS votes;
+-- DROP TABLE IF EXISTS candidates;
+-- DROP TABLE IF EXISTS parties;
+-- DROP TABLE IF EXISTS voters;
+
+-- CREATE TABLE parties (
+--   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+--   name VARCHAR(50) NOT NULL,
+--   description TEXT
+-- );
+
+-- CREATE TABLE candidates (
+--   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+--   first_name VARCHAR(30) NOT NULL,
+--   last_name VARCHAR(30) NOT NULL,
+--   party_id INTEGER,
+--   industry_connected BOOLEAN NOT NULL,
+--   CONSTRAINT fk_party
+--     FOREIGN KEY (party_id)
+--     REFERENCES parties(id)
+--     ON DELETE SET NULL
+-- );
+
+-- CREATE TABLE voters (
+--   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+--   first_name VARCHAR(30) NOT NULL,
+--   last_name VARCHAR(30) NOT NULL,
+--   email VARCHAR(50) NOT NULL,
+--   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- CREATE TABLE votes (
+--   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+--   voter_id INTEGER NOT NULL,
+--   candidate_id INTEGER NOT NULL,
+--   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+--   CONSTRAINT uc_voter UNIQUE (voter_id),
+--   CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+--   CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+-- );
+
+
+
+
+
+
+
+-- DROP TABLE IF EXISTS employee;
+-- DROP TABLE IF EXISTS roles;
+-- DROP TABLE IF EXISTS department;
+
+-- CREATE TABLE department (
+--   id int AUTO_INCREMENT PRIMARY KEY,
+--   name VARCHAR(30) NOT NULL
+-- );
+
+-- CREATE TABLE role (
+--   id int AUTO_INCREMENT PRIMARY KEY,
+--   title VARCHAR(30) NOT NULL,
+--   salary DECIMAL NOT NULL,
+--   department_id INTEGER NOT NULL,
+--   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)
+-- );
+
+-- CREATE TABLE employee (
+--   id int AUTO_INCREMENT PRIMARY KEY,
+--   first_name VARCHAR(30) NOT NULL,
+--   last_name VARCHAR(30) NOT NULL,
+--   manager_id INTEGER,
+--   role_id INTEGER NOT NULL,
+--   FOREIGN KEY (manager_id) REFERENCES employee(id),
+--   CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id)
+-- );
